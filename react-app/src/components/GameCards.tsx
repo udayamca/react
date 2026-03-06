3f8c41d24bf3f6f494d21bd148ee117dfeec14f6
udaya <udayamca@gmail.com>
2026-03-05 19:51:23 -0800

    adding emojis to heading

--- react-app/src/components/GameCards.tsx
+++ react-app/src/components/GameCards.tsx
@@ -3,6 +3,7 @@ import { Card, CardBody, Heading, HStack, Image } from "@chakra-ui/react";
 import PlatformListIcons from "./PlatformListIcons";
 import CriticScore from "./CriticScore";
 import getCroppedImageUrl from "@/services/image-url";
+import Emojis from "./Emojis";
 
 interface GameCardsProps {
   game: Game;
@@ -19,7 +20,10 @@ const GameCards = ({ game }: GameCardsProps) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
-        <Heading fontSize="2xl">{game.name}</Heading>
+        <Heading fontSize="2xl">
+          {game.name}
+          <Emojis rating={game.rating_top} />
+        </Heading>
       </CardBody>
     </Card>
   );
